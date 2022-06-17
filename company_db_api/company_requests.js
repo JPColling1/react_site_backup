6e6aca4076230ace758f58dd61bc2c526e909508
jcolling <jcolling@blueyeq.com>
2022-06-17 14:12:56 -0400

    created test code

---
+++ company_db_api/company_requests.js
@@ -0,0 +1,11 @@
+var SwaggerJsClient = require('swagger-js-client');
+
+var api = new SwaggerJsClient.DefaultApi()
+var callback = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully.');
+  }
+};
+api.apiDocJsonGet(callback);
