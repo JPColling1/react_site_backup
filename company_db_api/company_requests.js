b3fdc331a93a29e7dcf1bd756ba793335ea316f4
jcolling <jcolling@blueyeq.com>
2022-06-17 14:50:07 -0400

    added company_requests class

--- company_db_api/company_requests.js
+++ company_db_api/company_requests.js
@@ -1,11 +1,17 @@
-var SwaggerJsClient = require('swagger-js-client');
 
-var api = new SwaggerJsClient.DefaultApi()
-var callback = function(error, data, response) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('API called successfully.');
-  }
-};
-api.apiDocJsonGet(callback);
+export default class CompanyRequests{
+
+    testTheAPI = function(){
+        var SwaggerJsClient = require('swagger-js-client');
+
+        var api = new SwaggerJsClient.DefaultApi()
+        var callback = function(error, data, response) {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log('API called successfully.');
+        }
+        };
+        api.apiDocJsonGet(callback);
+    }
+}
