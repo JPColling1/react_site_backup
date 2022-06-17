188780b0ea2079c376054106e061d26f4cf65492
jcolling <jcolling@blueyeq.com>
2022-06-17 13:40:44 -0400

    embedded company API UI

--- src/pages/tools.js
+++ src/pages/tools.js
@@ -4,6 +4,7 @@ const Tools = () => {
   return (
     <div>
       <h1>Tools</h1>
+      <iframe src="http://127.0.0.1:8050" title="Manage Companies" height={450} width={1000}></iframe>
     </div>
   );
 };
