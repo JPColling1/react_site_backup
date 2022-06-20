320a38843e6769352dd3fe7b148b10b93df86163
jcolling <jcolling@blueyeq.com>
2022-06-20 09:41:13 -0400

    completed test function

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
+//import CompanyRequests from '../company_db_api/company_requests';
+  
+//let requests = new CompanyRequests();
+
+function testFunction(){
+    console.log("activated");
+}
 
 export default class Button extends React.Component{
+    
     render(){
-        return  <button type="button">Click Me!</button> 
+        return  <button onClick={testFunction} type="button">Click Me!</button> 
     }
 }
