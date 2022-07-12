5096f3183df6c8225763e4e4c208cd8ec6a0c899
jcolling <jcolling@blueyeq.com>
2022-07-12 15:17:48 -0400

    starting to draw

--- src/components/AudioController/FrequencyChart/index.js
+++ src/components/AudioController/FrequencyChart/index.js
@@ -1,6 +1,15 @@
 import React, {useState, useEffect, useRef } from "react";
 import Dygraph from 'dygraphs';
+import './dygraph.css';
 
+//Highlight a band given a start and end frequency
+function drawHighlight(startFreq, endFreq, graph) {
+  var left = graph.toDomXCoord(startFreq);
+  var right = graph.toDomXCoord(endFreq);
+
+  canvas.fillStyle = "rgba(255, 255, 102, 0.5)";
+  canvas.fillRect(left, top, right - left, bottom - top);
+}
 
 export const FrequencyChart = (props) => {
 
@@ -31,6 +40,10 @@ export const FrequencyChart = (props) => {
         labelsDiv: document.getElementById("frequency"),
         height: 500,
         width: 1000,
+        title: "Frequency Spectrum",
+        xlabel: "frequency(Hz)",
+        ylabel: "level(dB)",
+        y2label: "will you show?",
       }
       // CSV or path to a CSV file.
       // "Date,Temperature\n" +
@@ -39,11 +52,12 @@ export const FrequencyChart = (props) => {
       // "2008-05-09,80\n"
   
     );
+    drawHighlight(1000, 2000, g);
   }
 
   return(<div>
-          <div id="frequency"></div>
           <div id="graph"></div>
+          <div id="frequency"></div>
         </div>)
 }
 
