a21dd6e052801f543112ede28fa30d9738c36cf1
jcolling <jcolling@blueyeq.com>
2022-07-19 14:21:27 -0400

    support adding files from onedrive to graph

--- src/components/AudioController/index.js
+++ src/components/AudioController/index.js
@@ -1,140 +1,127 @@
 import React, { useRef } from "react";
 import { Oscillosope, draw, set_CanvasCtx } from './Oscilloscope/index'
 import { FrequencyChart } from "./FrequencyChart";
+import PathSetter from "./PathSetter";
 import Snare1 from './Snare1.wav';
 
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 
 const AudioController = () => {
     //component state
-    const [playing, setPlaying] = React.useState(false);
-    const [time, setTime] = React.useState(0);
+    // const [playing, setPlaying] = React.useState(false);
+    // const [time, setTime] = React.useState(0);
+    const [filePath, setFilePath] = React.useState(0);
+    const [readyForData, setReadyforData] = React.useState(false);
 
     //component props
-    const audioRef = useRef(null);
+    // const audioRef = useRef(null);
 
-    const audioContext = new AudioContext();
-    const analyzer1 = useRef(null);
-    const analyzer2 = useRef(null);
-    const gainNode = useRef(null);
-    var audioElement = useRef(null);
-    var track = useRef(null);
+    // const audioContext = new AudioContext();
+    // const analyzer1 = useRef(null);
+    // const analyzer2 = useRef(null);
+    // const gainNode = useRef(null);
+    // var audioElement = useRef(null);
+    // var track = useRef(null);
 
-    var bufferLength = useRef(null);
-    var dataArray1 = useRef(null);
-    var dataArray2 = useRef(null);
+    // var bufferLength = useRef(null);
+    // var dataArray1 = useRef(null);
+    // var dataArray2 = useRef(null);
 
-    const sampleRate = 44100;
+    // const sampleRate = 44100;
 
     //create audio
-    const create_audio = () => {
-        analyzer1.current = audioContext.createAnalyser();
-        analyzer2.current = audioContext.createAnalyser();
-        gainNode.current = audioContext.createGain();
-        audioElement.current = audioRef.current;
-        track.current = audioContext.createMediaElementSource(audioElement.current);
-        track.current.connect(analyzer1.current).connect(analyzer2.current).connect(gainNode.current).connect(audioContext.destination);
+    // const create_audio = () => {
+    //     analyzer1.current = audioContext.createAnalyser();
+    //     analyzer2.current = audioContext.createAnalyser();
+    //     gainNode.current = audioContext.createGain();
+    //     audioElement.current = audioRef.current;
+    //     track.current = audioContext.createMediaElementSource(audioElement.current);
+    //     track.current.connect(analyzer1.current).connect(analyzer2.current).connect(gainNode.current).connect(audioContext.destination);
 
-        audioElement.current.addEventListener('ended', () => {
-            console.log("playback ended");
-            setPlaying(false);
-        }, false);
+    //     audioElement.current.addEventListener('ended', () => {
+    //         console.log("playback ended");
+    //         setPlaying(false);
+    //     }, false);
 
-        console.log("starting audio");
+    //     console.log("starting audio");
 
-        //set oscilloscope canvas
-        //set_CanvasCtx();
-        //set_chart_ctx();
-    }   
+    //     //set oscilloscope canvas
+    //     //set_CanvasCtx();
+    //     //set_chart_ctx();
+    // }   
 
     //play or pause audio
-    const play_pause = () => {
-        //if context is suspended, resume
-        if (audioContext.state === 'suspended') {
-            audioContext.resume();
-        }
+    // const play_pause = () => {
+    //     //if context is suspended, resume
+    //     if (audioContext.state === 'suspended') {
+    //         audioContext.resume();
+    //     }
 
-        // play or pause track depending on state
-        if (playing === false) {
-            console.log("playing");
-            audioElement.current.play();
-            setPlaying(true);
-            // console.log("time is at " + audioElement.current.currentTime);
-            // audioElement.current.currentTime = 0;
-            // console.log(audioElement.current.currentTime);
-        } else if (playing === true) {
-            console.log("pausing");
-            audioElement.current.pause();
-            setPlaying(false);
-        }
-    }
+    //     // play or pause track depending on state
+    //     if (playing === false) {
+    //         console.log("playing");
+    //         audioElement.current.play();
+    //         setPlaying(true);
+    //         // console.log("time is at " + audioElement.current.currentTime);
+    //         // audioElement.current.currentTime = 0;
+    //         // console.log(audioElement.current.currentTime);
+    //     } else if (playing === true) {
+    //         console.log("pausing");
+    //         audioElement.current.pause();
+    //         setPlaying(false);
+    //     }
+    // }
 
     //adjust gain
-    const adjust_volume = (value) => {
-        gainNode.current.gain.value = value;
-    }
+    // const adjust_volume = (value) => {
+    //     gainNode.current.gain.value = value;
+    // }
 
-    const adjust_time = (value) => {
-        fetch("/currentTime", {
-            method:"POST",
-            body: JSON.stringify({"currTime": value}),
-            headers: new Headers({ 
-                'Content-Type': 'application/json',
-            }),
+    // const adjust_time = (value) => {
+    //     fetch("/currentTime", {
+    //         method:"POST",
+    //         body: JSON.stringify({"currTime": value}),
+    //         headers: new Headers({ 
+    //             'Content-Type': 'application/json',
+    //         }),
             
-        }).then(response  => {
-            return response.json();
-        }).then(data => {
-            console.log(data);
-        }).catch(error => console.log(error));
+    //     }).then(response  => {
+    //         return response.json();
+    //     }).then(data => {
+    //         console.log(data);
+    //     }).catch(error => console.log(error));
 
-        value += 1;
-        //setTime(value);
-        //console.log(time);
+    //     value += 1;
+    //     //setTime(value);
+    //     //console.log(time);
+    // }
+    const handlePathChange = (e) => {
+        setReadyforData(false);
+        setFilePath(e.target.value);
     }
 
-    //get fft data
-    const get_datapoints = () => {
-        analyzer1.current.fftSize = 2048;
-        analyzer2.current.fftSize = 2048;
-        bufferLength.current = analyzer1.current.frequencyBinCount;
-        dataArray1.current = new Uint8Array(bufferLength.current);
-        dataArray2.current = new Uint8Array(bufferLength.current);
-        analyzer1.current.getByteTimeDomainData(dataArray1.current);
-        analyzer2.current.getByteFrequencyData(dataArray2.current);
-    
-        // console.log(dataArray1);
-        // console.log(dataArray2);
-
-        //draw(analyzer1.current, bufferLength.current, dataArray1.current);
-        //create_dataset(analyzer2.current, dataArray2.current, sampleRate);
-        //initData(analyzer2.current, dataArray2.current);
-    }
-
-    
     //html components
     return (<div>    
-        <audio src={Snare1} ref={audioRef} />
+        <div>Paste file path here</div>
+        <input onChange={(e) => handlePathChange(e)}></input>
+        <PathSetter filePath={filePath} readyData={setReadyforData}/>
 
-        <button onClick={() => {create_audio()}}>Start Audio</button>
-        <button onClick={() => {get_datapoints()}}>get fft data</button>
-        <button onClick={() => {play_pause()}}>
+        {/* <audio src={Snare1} ref={audioRef} /> */}
+
+        {/* <button onClick={() => {create_audio()}}>Start Audio</button> */}
+        {/* <button onClick={() => {get_datapoints()}}>get fft data</button> */}
+        {/* <button onClick={() => {play_pause()}}>
             <span>Play/Pause</span>
-        </button>
-        <input type="range" min="0" max="2" step="0.01" onChange={event => adjust_volume(event.target.value)}></input>
-        <input type="range" min="0" max="1" step="0.01" onChange={event => adjust_time(event.target.value)}></input>
+        </button> */}
+        {/* <input type="range" min="0" max="2" step="0.01" onChange={event => adjust_volume(event.target.value)}></input> */}
+        {/* <input type="range" min="0" max="1" step="0.01" onChange={event => adjust_time(event.target.value)}></input> */}
 
-        <Oscillosope />
-        <FrequencyChart />
+        <Oscillosope readyForData={readyForData}/>
+        <FrequencyChart readyForData={readyForData}/>
         
       </div>);
 }
 
-//create frequency domain data
-// function create_dataset(analyzer2, dataArray2, sampleRate){
-//     setTimeout(create_dataset, 17, analyzer2, dataArray2, sampleRate);
-//     analyzer2.getByteFrequencyData(dataArray2);
-// }    
 
 
 export default AudioController;
