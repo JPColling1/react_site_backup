089f38aa5d22ba06b0a0896ac02c0b783426a33d
jcolling <jcolling@blueyeq.com>
2022-07-22 12:13:31 -0400

    render options only when toggled

--- src/components/AudioViewer/PartFetcher/SearchRender/index.js
+++ src/components/AudioViewer/PartFetcher/SearchRender/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import PartItem from "./PartItem";
 
-const SearchRender = (props) => {
+const SearchRender = ({ toggled, partsList, value }) => {
 
-    return(<div>
-        {
-            props.partsList.map((item) => (
-                <PartItem name={item.partNum} key={item.partId}/>
-            ))
-        }
-        </div>
-    )
+    const filteredList = partsList.filter(item => item.partNum.toString().toLowerCase().startsWith(value.toLowerCase()))
+
+    if (toggled === true){
+        return(<div>
+            {
+                filteredList.map((item) => (
+                    <PartItem name={item.partNum} key={item.partId}/>
+                ))
+            }
+            </div>
+        )
+    } else {
+        return (<div />)
+    }
+
 }
 
 export default SearchRender;
