1c329e726986f3135a9089fcfd5164951f10ddc6
jcolling <jcolling@blueyeq.com>
2022-07-22 17:00:48 -0400

    pass array of PartItem refs to PartFetcher

--- src/components/AudioViewer/PartFetcher/SearchRender/index.js
+++ src/components/AudioViewer/PartFetcher/SearchRender/index.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import PartItem from "./PartItem";
 
-const SearchRender = ({ toggled, partsList, value }) => {
+const SearchRender = ({ toggled, partsList, value, setCurrentId, setRefList }) => {
 
     const filteredList = partsList.filter(item => item.partNum.toString().toLowerCase().startsWith(value.toLowerCase()))
+    var refList = [];
+
+    const addRef = (ref, index) => {
+        refList[index] = ref;
+        setRefList(refList);
+    }
 
     if (toggled === true){
         return(<div>
             {
-                filteredList.map((item) => (
-                    <PartItem name={item.partNum} key={item.partId}/>
+                filteredList.map((item, index) => (
+                    <PartItem name={item.partNum} key={item.partId} setCurrentId={setCurrentId} partId={item.partId} addRef={addRef} index={index}/>
                 ))
             }
             </div>
