24f585efda369e6875e95d16c9ff60cbabaf818c
jcolling <jcolling@blueyeq.com>
2022-07-22 17:02:29 -0400

    detect clicks outside of search box and results to close search dropdown

--- src/components/AudioViewer/PartFetcher/index.js
+++ src/components/AudioViewer/PartFetcher/index.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import SearchRender from "./SearchRender";
 import SearchBox from "./SearchBox";
+import SpeedInput from "./SpeedInput";
 
 //fetch part objects with the newDB api and return them as json objects in a list
 
@@ -9,6 +10,19 @@ const PartFetcher = () => {
     const [partsList, setPartsList] = useState([]);
     const [value, setValue] = useState("");
     const [toggled, setToggled] = useState(false);
+    const [speed, setSpeed] = useState(0);
+    const [currentId, setCurrentId] = useState(0);
+
+
+    var inputRef1 = useRef(null);
+    const setInputRef = (ref) => {
+        inputRef1.current = ref;
+    }
+
+    var refList = useRef(null);
+    const setRefList = (refs) => {
+        refList.current = refs;
+    }
 
     useEffect(() => {
         //fetch parts and put response in parts List
@@ -23,9 +37,38 @@ const PartFetcher = () => {
         
     }, [])
 
+    useEffect(() => {
+        function handleClickOutside(event) {
+            //check if any partItems are within click bounds
+            let inList = false;
+            //{debugger}
+            if (refList.current !== null){
+                for (let i = 0; i < refList.current.length; i++){
+                    if (refList.current[i].contains(event.target)){
+                        inList = true;
+                    }
+                }
+            }
+            if (inputRef1.current && !inputRef1.current.contains(event.target) && !inList) {
+                setToggled(false);
+            }
+        }
+        // Bind the event listener
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+        };
+    })
+
+    const handleSelection = (idNum) => {
+        var promise = setCurrentId(idNum);
+    }
+
     return<div id="myDropdown" className="dropdown-content">
-        <SearchBox setValue={setValue} setToggled={setToggled}/>
-        <SearchRender toggled={toggled} partsList={partsList} value={value}/>
+        <SpeedInput setSpeed={setSpeed}/>
+        <SearchBox setValue={setValue} setToggled={setToggled} setInputRef={setInputRef}/>
+        <SearchRender toggled={toggled} partsList={partsList} value={value} setCurrentId={handleSelection} setRefList={setRefList}/>
     </div>
 }
 
