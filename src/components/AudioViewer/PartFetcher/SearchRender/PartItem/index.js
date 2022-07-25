882948991265491b04f76df047768131e8524f90
jcolling <jcolling@blueyeq.com>
2022-07-25 11:41:23 -0400

    incorporate handleChange prop to update highlights on click

--- src/components/AudioViewer/PartFetcher/SearchRender/PartItem/index.js
+++ src/components/AudioViewer/PartFetcher/SearchRender/PartItem/index.js
@@ -1,13 +1,35 @@
 import React, { useEffect, useRef } from "react";
 
-const PartItem = ( { name, setCurrentId, partId, addRef, index} ) => {
+//send currently selected part ID to backend for processing
+const postCurrentID = (partId, handleChanges) => {
+    fetch("/partSet", {
+        method:"POST",
+        body: JSON.stringify({"partId": partId}),
+        headers: new Headers({ 
+            'Content-Type': 'application/json',
+        }),
+    }).then(response  => {
+        return response.json();
+    }).then(data => {
+        handleChanges();
+        console.log(data);
+    }).catch(error => console.log(error));
+}
+
+//list item component
+const PartItem = ( { name, partId, addRef, index, handleChanges } ) => {
     var itemRef = useRef(null);
 
     useEffect(() => {
         addRef(itemRef.current, index);
     });
 
-    return<div ref={itemRef} onClick={() => setCurrentId(partId)}>{name}</div>
+    //pass part ID to post method
+    const handleSelection = () => {
+        postCurrentID(partId, handleChanges);
+    }
+
+    return<div ref={itemRef} onClick={handleSelection}>{name}</div>
 }
 
 export default PartItem;
