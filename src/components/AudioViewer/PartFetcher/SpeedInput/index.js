23477d1e7ec069c7ed40c2caaf750a07dcbf846b
jcolling <jcolling@blueyeq.com>
2022-07-25 11:42:17 -0400

    update highlights on value change

--- src/components/AudioViewer/PartFetcher/SpeedInput/index.js
+++ src/components/AudioViewer/PartFetcher/SpeedInput/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const SpeedInput = ( { setSpeed } ) => {
-    return (<input placeholder="speed (rpm)" onChange={(e) => setSpeed(e.target.value)}/>)
+//pass new speed to backend for processing
+const postSpeedVal = (speed, handleChanges) => {
+    fetch("/speedSet", {
+            method:"POST",
+            body: JSON.stringify({"speed": speed}),
+            headers: new Headers({ 
+                'Content-Type': 'application/json',
+            }),
+            
+        }).then(response  => {
+            return response.json();
+        }).then(data => {
+            console.log(data);
+            handleChanges();
+        }).catch(error => console.log(error));
+}
+
+//speed input box component
+const SpeedInput = ( { handleChanges } ) => {
+    return (<input placeholder="speed (rpm)" onChange={(e) => postSpeedVal(e.target.value, handleChanges)}/>)
 }
 
 export default SpeedInput;
